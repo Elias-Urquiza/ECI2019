25121fe3bdf63611e72f284d914c8cecb179992a
QuanticSausage <jonatan.dan@hotmail.com>
2019-08-16 14:25:21 -0300

    el pixel tiene en las ultimas 2 posiciones 2 B, si el programa escupe entero el archivo hasta el final esta todo bien

--- openImage.c
+++ openImage.c
@@ -5,7 +5,7 @@
 int main(int argc, int* argv[]){
 
 	FILE *file_stream = NULL;
-	char* buffer;
+	int* buffer;
 	char *mode = "rb"; // "rb" is read/binary mode
 
 	file_stream = fopen((char *) argv[1], mode); // Read filename from the parameter that was given when the program was called
@@ -19,11 +19,10 @@ int main(int argc, int* argv[]){
 	int file_size = ftell(file_stream); // Offset from start
 	rewind(file_stream); // Go to start
 
-	buffer = (char *)malloc((file_size+1)*sizeof(char)); //Calculate file size
+	buffer = (int *)malloc((file_size+1)*sizeof(int)); //Calculate file size
 	fread(buffer, file_size, 1, file_stream); // Copy the file to the buffer
 	fclose(file_stream);
-	
-	fwrite(buffer, 1, sizeof(buffer), stdout);
+	fwrite( buffer, file_size, 1, stdout); // print to stdout: file_size times 1 Byte from the pointer 'buffer'
 
 	free(buffer);
 	return 0;
