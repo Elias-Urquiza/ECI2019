94c36a15a091e7a43d1e59b6ee1475371969e2d4
QuanticSausage <jonatan.dan@hotmail.com>
2019-08-17 11:10:49 -0300

    De la forma escrita podemos manipular bytes del archivo, falta ver desde donde comienzan los pixeles

--- openImage.c
+++ openImage.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 int main(int argc, int* argv[]){
 
 	FILE *file_stream = NULL;
-	int* buffer;
-	char *mode = "rb"; // "rb" is read/binary mode
+	uint8_t* buffer; // We use integers with Byte length to have an easier manipulation of the file
+	char *mode = "rb"; // rb is read in binary mode
 
 	file_stream = fopen((char *) argv[1], mode); // Read filename from the parameter that was given when the program was called
 	
@@ -19,11 +20,19 @@ int main(int argc, int* argv[]){
 	int file_size = ftell(file_stream); // Offset from start
 	rewind(file_stream); // Go to start
 
-	buffer = (int *)malloc((file_size+1)*sizeof(int)); //Calculate file size
+	buffer = (uint8_t *)malloc((file_size+1)*sizeof(uint8_t)); //Calculate file size
 	fread(buffer, file_size, 1, file_stream); // Copy the file to the buffer
-	fclose(file_stream);
-	fwrite( buffer, file_size, 1, stdout); // print to stdout: file_size times 1 Byte from the pointer 'buffer'
 
+	fclose(file_stream);
+
+// Test to see how to manipulate the file byte for byte
+	fwrite( buffer, file_size, 1, stdout); // print to stdout: file_size times 1 Byte from the pointer 'buffer'
+	printf("\n \n");
+	
+	buffer[0] = 0x4d;
+
+	fwrite( buffer, file_size, 1, stdout);
 	free(buffer);
 	return 0;
+
 }
