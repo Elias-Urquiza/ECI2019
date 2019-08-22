d7c20cf16d863ef856f681669539af694628e825
QuanticSausage <jonatan.dan@hotmail.com>
2019-08-22 11:23:47 -0300

    Prepare el script para recibir los argumentos por parte del usuario, lo unico que queda hacer es que cada uno escriba su modulo correspondiente, fijense en la carpeta modules

--- main.c
+++ main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include "libbmp.h"
+#include "./modules/hideInformation.c"
+#include "./modules/unhideInformation.c"
+#include "./modules/benchmarking.c"
 
 void printUsage(){
 		printf("usage:\nfirst parameter: write a number from 1 to 3.\n1 to hide information\n2 to extract information\n3 to do benchmarking\n");
 }
 
 int main(int argc, char* argv[]){
-int mode = 0;
+
+int mode;	// The code from here it's only to ensure the usage
 if(argc==1){
 	printUsage();
 	return 0;
 }
+
 if(argc > 2){
 	printUsage();
 	return 0;
@@ -29,8 +33,20 @@ if(argc > 2){
 	return 0;
 	}
 }
-
+/*
 	for(int i=1;i<argc;i++){
 	printf("%s \n", argv[i]);
 	}
+*/
+	if(mode == 1){
+	printf("You have chosen to hide information\n");
+	start_hideInformation();	// Here goes the module written by Alex, this function must be written in hideinformation.c
+	} else if(mode == 2){
+		printf("You have chosen to unhide information\n");
+		start_unhideInformation(); // Here goes the module written by Me, written in unhideinformation.c
+	} else if(mode == 3){
+			printf("You have chosen to do benchmarking\n");
+			start_benchmarking();	// Here goes the module written by Tomi, David and Elias. Defined in benchmarking
+	}
+return 0;
 }
---
+++ modules/benchmarking.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "libbmp.h"
+
+void start_benchmarking(){
+
+}
